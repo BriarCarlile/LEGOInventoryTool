d978df8fa88f60bb67de82bc69aeb7ad67a02201
Briar Carlile <briarcarlile@Briars-MacBook-Pro.local>
2025-03-23 18:32:15 -0600

    Initial commit, Springboot prefab

---
+++ src/main/java/com/legoinventorytool/api/LegoInventoryToolApplication.java
@@ -0,0 +1,13 @@
+package com.legoinventorytool.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoInventoryToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoInventoryToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legoinventorytool/api/LegoInventoryToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legoinventorytool.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoInventoryToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
