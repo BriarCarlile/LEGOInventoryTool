c0cae58f767d6b2e013039c434ec533c7c2a2823
BriarCarlile <briar.carlile@gmail.com>
2025-03-29 18:49:13 +0000

    Add jakarta persistence

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
